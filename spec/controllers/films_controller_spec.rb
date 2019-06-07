a87869c08b8900d8700675584d382f8993f19ad1
Nikodem Górak <nikos83@gmail.com>
2019-06-08 00:25:59 +0200

    Add custom validator to validate active storage image

--- spec/controllers/films_controller_spec.rb
+++ spec/controllers/films_controller_spec.rb
@@ -9,11 +9,4 @@ RSpec.describe FilmsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe 'GET #show' do
-    it 'returns http success' do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
 end
