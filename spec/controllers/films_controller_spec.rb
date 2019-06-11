fef7ae90b08afe9acf4636011dc368c93ba96bec
Nikodem Górak <nikos83@gmail.com>
2019-06-11 13:07:56 +0200

    Change spec

--- spec/controllers/films_controller_spec.rb
+++ spec/controllers/films_controller_spec.rb
@@ -3,10 +3,5 @@
 require 'rails_helper'
 
 RSpec.describe FilmsController, type: :controller do
-  describe 'GET #index' do
-    it 'returns http error must be logged in' do
-      get :index
-      expect(response).to have_http_status(302)
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
