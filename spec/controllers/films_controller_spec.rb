d70e50f9bdabf7725390bead22a09eb4cc3c44cf
Nikodem Górak <nikos83@gmail.com>
2019-06-11 13:05:57 +0200

    Ajust width of film

--- spec/controllers/films_controller_spec.rb
+++ spec/controllers/films_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe FilmsController, type: :controller do
   describe 'GET #index' do
     it 'returns http error must be logged in' do
       get :index
-      expect(response).to have_http_status(:redirect)
+      expect(response).to have_http_status(302)
     end
   end
 end
