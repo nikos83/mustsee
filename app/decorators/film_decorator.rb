8d1fa076cb9e5ddc2f70ff84d7741b387b5da305
Nikodem Górak <nikos83@gmail.com>
2019-06-23 00:04:57 +0200

    Fix typo on view

--- app/decorators/film_decorator.rb
+++ app/decorators/film_decorator.rb
@@ -6,7 +6,7 @@ class FilmDecorator < Draper::Decorator
   def delete_link
     - if object.film_link.attached?
         h.link_to 'Remove Film', h.delete_film_link_attachment_film_url(object.film_link.id),
-                  method: :delete, data: { confirm: 'are_you_sure' }, class: 'btn btn-warning'
+                  method: :delete, data: { confirm: 'Are you sure?' }, class: 'btn btn-warning'
       else
         'No film attached'
       end
