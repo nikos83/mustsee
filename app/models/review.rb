a4618123f0db7cc5caadbdaa84ee9645acdb05df
Nikodem Górak <nikos83@gmail.com>
2019-09-28 17:52:50 +0200

    Add limitation one user one comment

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,6 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :film
   validates :rating, presence: true
-  # validates :user_id, uniqueness:
-  # { scope: :film_id, message: "You're only allowed to have one comment per film." }
+  validates :user_id, uniqueness:
+  { scope: :film_id, message: "You're only allowed to have one comment per film." }
 end
