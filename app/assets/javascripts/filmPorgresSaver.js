c3114a86432ec7a53002b40da162fa3d7753c0b8
Nikodem Gorak <nikos83@gmail.com>
2019-11-05 14:30:44 +0100

    Add if condition for places without name

--- app/assets/javascripts/filmPorgresSaver.js
+++ app/assets/javascripts/filmPorgresSaver.js
@@ -1,14 +1,16 @@
 
 var myOnload =   window.onload = (event) => {
-  window.vid = document.getElementById('mustsee-video');
-  window.getName = document.querySelector('.film-name').textContent;
-  if (!(localStorage.getItem(window.getName) ===  null)) {
-    window.vid.currentTime = localStorage.getItem(window.getName);
-  }
-  window.vid .onpause = function() {onPauseFunction()};
+  if (!(document.getElementById('mustsee-video') === null )) {
+    window.vid = document.getElementById('mustsee-video');
+    window.getName = document.querySelector('.film-name').textContent;
+    if (!(localStorage.getItem(window.getName) ===  null)) {
+      window.vid.currentTime = localStorage.getItem(window.getName);
+    }
+    window.vid .onpause = function() {onPauseFunction()};
 
-  function onPauseFunction() {
-    localStorage.setItem(window.getName, window.vid.currentTime);
+    function onPauseFunction() {
+      localStorage.setItem(window.getName, window.vid.currentTime);
+    }
   }
 };
 $(document).on('turbolinks:load', myOnload);
