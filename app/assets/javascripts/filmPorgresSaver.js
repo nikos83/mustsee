959493d61c0f693d7eb77a77b13a625896952d44
Nikodem Gorak <nikos83@gmail.com>
2019-11-05 14:01:21 +0100

    Change js condition

--- app/assets/javascripts/filmPorgresSaver.js
+++ app/assets/javascripts/filmPorgresSaver.js
@@ -2,9 +2,7 @@
 var myOnload =   window.onload = (event) => {
   window.vid = document.getElementById('mustsee-video');
   window.getName = document.querySelector('.film-name').textContent;
-  if (localStorage.getItem(window.getName) ===  null) {
-  }
-  else{
+  if (!(localStorage.getItem(window.getName) ===  null)) {
     window.vid.currentTime = localStorage.getItem(window.getName);
   }
   window.vid .onpause = function() {onPauseFunction()};
