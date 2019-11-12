3223ab461a6b6a714686cf0d40d0789123698b20
Nikodem Górak <nikos83@gmail.com>
2019-11-12 19:03:53 +0100

    Add default url to env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,7 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  config.action_mailer.default_url_options = { host: 'mustsee.x25.pl', port: 80 }
 
   if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
