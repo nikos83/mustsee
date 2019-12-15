3d5add6bda174ecb5a34106204ddb48900ca4cc0
Nikodem Górak <nikos83@gmail.com>
2019-12-15 18:47:14 +0100

    Add tests for email

---
+++ spec/mailers/admin_notifier_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe AdminNotifierMailer, type: :mailer do
+  describe 'admin information' do
+    let(:user) { create :user, :confirmed }
+    let(:admin) { create :user, :admin, :confirmed }
+    let(:mail) { AdminNotifierMailer.notify_admin(admin, user).deliver_now }
+
+    it 'renders the subject' do
+      expect(mail.subject).to eq('Notification')
+    end
+
+    it 'renders the admin email' do
+      expect(mail.to).to eq([admin.email])
+    end
+
+    it 'renders the sender email' do
+      expect(mail.from).to eq(['from@example.com'])
+    end
+
+    it 'assigns user email' do
+      expect(mail.body.encoded).to match(user.email)
+    end
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let!(:admin) { create :user, :admin, :confirmed }
+  let!(:user) { create :user }
   it { should validate_presence_of(:email) }
+
+  it 'sends an email to admin' do
+    expect { AdminNotifierMailer.notify_admin(admin, user).deliver_now }
+      .to change { ActionMailer::Base.deliveries.count }.by(1)
+  end
 end
