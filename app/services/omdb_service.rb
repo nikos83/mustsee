63bb2b56dc513084bd30d767b369cae7d0b1dd28
Nikodem Górak <nikos83@gmail.com>
2020-01-22 00:01:15 +0100

    Add omdb service to handle api requests

---
+++ app/services/omdb_service.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class OmdbService
+  include HTTParty
+  base_uri 'www.omdbapi.com'
+
+  def initialize(title, year)
+    @options = { query: { s: title, y: year, apikey: Rails.application.credentials.dig(:omdb, :apikey) } }
+  end
+
+  def search
+    self.class.get('', @options)
+  end
+end
