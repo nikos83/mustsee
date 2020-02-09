aed667d6d664112c7f923524a160d15386bc13be
Nikodem Górak <nikos83@gmail.com>
2020-02-09 19:16:47 +0100

    Delete search controller and views

--- app/controllers/omdb_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class OmdbController < ApplicationController
-  def index
-    @film = OmdbService.new(params[:search], params[:year])
-  end
-
-  def search; end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'omdb/index'
-  get 'omdb/search'
   resources :films do
     resources :attachments, only: :destroy
     resources :reviews
-    resources :omdb
+    resources :omdb, only: %i[index search]
   end
   devise_for :users, controllers: {
     registrations: 'users/registrations'
