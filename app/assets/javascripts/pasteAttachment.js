d40d9f483dec4cd17c341c496cdd89630173aaa4
Nikodem Górak <nikos83@gmail.com>
2020-02-16 22:49:02 +0100

    Delete unused code

--- app/assets/javascripts/pasteAttachment.js
+++ app/assets/javascripts/pasteAttachment.js
@@ -3,10 +3,6 @@ var myOnload =   window.onload = (event) => {
     const form = document.getElementById('new_film');
     const fileInput = document.getElementById('film_cover_img');
 
-    fileInput.addEventListener('change', () => {
-      console.log('hello');
-    });
-
     window.addEventListener('paste', e => {
       fileInput.files = e.clipboardData.files;
     });
